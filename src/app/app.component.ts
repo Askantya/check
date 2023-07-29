fa47b6a42222239bea51f4846da51d9de3c4a6d2
Elizaveta Ivanova <lizuha1860@gmail.com>
2023-07-29 03:02:39 +0300

    new project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.ts',
+    styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent { 
+    
+}
